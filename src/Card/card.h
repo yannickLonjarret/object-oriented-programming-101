a470a0951ed9bae4cbf68d0d4a36729d026e663a
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-01 21:35:29 +0200

    Add base fields for a card

--- src/Card/card.h
+++ src/Card/card.h
@@ -1,4 +1,12 @@
 #ifndef CARD_H
 #define CARD_H
 
+enum class CardColors { EMPTY, GREEN, BLUE, RED, YELLOW, PURPLE, BROWN };
+
+class Card {
+  private:
+    const int value;
+    const CardColors color;
+};
+
 #endif
