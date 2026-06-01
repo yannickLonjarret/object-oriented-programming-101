98b0c2671fdf3e50fb10b64f229c419746891359
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-01 21:14:16 +0200

    Add instructions for Player class fields

--- src/Player/player.h
+++ src/Player/player.h
@@ -1,4 +1,14 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
+
+class Player {
+  private:
+    /*
+    player name, cards in hand, number of played tactical cards, etc.
+    */
+  public:
+};
+
 #endif
