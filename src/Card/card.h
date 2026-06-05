0d8be3fe7610065124206efe69de0b820f7b7b32
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-05 22:24:14 +0200

    Add Cards namespace, create a Card base class and ClanCard class

--- src/Card/card.h
+++ src/Card/card.h
@@ -1,12 +1,20 @@
 #ifndef CARD_H
 #define CARD_H
 
+namespace Cards {
 enum class CardColors { EMPTY, GREEN, BLUE, RED, YELLOW, PURPLE, BROWN };
 
-class Card {
+class Card {};
+
+class ClanCard : public Card {
   private:
     const int value;
     const CardColors color;
+
+  public:
+    int GetValue() const { return value; }
+    CardColors GetColor() const { return color; }
 };
+} // namespace Cards
 
 #endif
