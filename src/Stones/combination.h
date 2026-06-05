df554cabb8348aae4647aa5863873c4c32263814
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-05 22:37:41 +0200

    Define CardCombination constructor with ClanCards as input

--- src/Stones/combination.h
+++ src/Stones/combination.h
@@ -1,15 +1,18 @@
 #ifndef COMBINATION_H
 #define COMBINATION_H
 
+#include <numeric>
 #include <vector>
 
 #include "card.h"
+
+using namespace Cards;
 
 namespace Combination {
 
 enum class Type { SUM = 100, SERIE = 200, COLOR = 300, KIND = 400, COLORED_SERIE = 500 };
 
-Type computeCombinationType(const std::vector<Card>& cards);
+Type computeCombinationType(const std::vector<ClanCard>& cards);
 
 class CardCombination {
   private:
@@ -18,6 +21,11 @@ class CardCombination {
 
   public:
     CardCombination(Type type, int sum) : combinationType(type), sumOfCards(sum) {};
+
+    CardCombination(const std::vector<ClanCard>& cards) {
+        combinationType = computeCombinationType(cards);
+        sumOfCards = std::accumulate(cards.begin(), cards.end(), 0, [](int sum, const ClanCard& c) { return sum + c.GetValue(); });
+    }
 
     ~CardCombination() = default;
 };
