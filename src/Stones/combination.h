2d59d53fe85f8d2632bb50d6ae4e629d2cc010bf
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-05 21:38:41 +0200

    Add namespace, combination type and class

--- src/Stones/combination.h
+++ src/Stones/combination.h
@@ -1,4 +1,19 @@
 #ifndef COMBINATION_H
 #define COMBINATION_H
 
+namespace Combination {
+
+enum class Type { SUM = 100, SERIE = 200, COLOR = 300, KIND = 400, COLORED_SERIE = 500 };
+
+class CardCombination {
+  private:
+    Type combinationType;
+    int sumOfCards;
+
+  public:
+    CardCombination(Type type, int sum) : combinationType(type), sumOfCards(sum) {};
+};
+
+} // namespace Combination
+
 #endif
