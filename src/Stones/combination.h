37a99963feb29395ec84230c5d00c7ca17b04fdd
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-05 21:55:09 +0200

    Declare CardCombination type idnetifier

--- src/Stones/combination.h
+++ src/Stones/combination.h
@@ -1,9 +1,15 @@
 #ifndef COMBINATION_H
 #define COMBINATION_H
 
+#include <vector>
+
+#include "card.h"
+
 namespace Combination {
 
 enum class Type { SUM = 100, SERIE = 200, COLOR = 300, KIND = 400, COLORED_SERIE = 500 };
+
+Type computeCombinationType(const std::vector<Card>& cards);
 
 class CardCombination {
   private:
