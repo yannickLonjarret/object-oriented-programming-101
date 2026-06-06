ba997f2647eb72553f832fc4b0a00380baf3de98
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-06 21:38:12 +0200

    Add combination.cpp file and prepare function definition

---
+++ src/Stones/combination.cpp
@@ -0,0 +1,12 @@
+#include "combination.h"
+
+namespace Combination {
+Type computeCombinationType(const std::vector<ClanCard>& cards) {}
+
+bool isSerie(std::vector<ClanCard> cards) {}
+bool isColor(const std::vector<ClanCard>& cards) {}
+bool isKind(const std::vector<ClanCard>& cards) {}
+bool isColoredSerie(const std::vector<ClanCard>& cards) {}
+
+void CardCombination::combinationTypeConversion(const Type& convertTo) {}
+} // namespace Combination
