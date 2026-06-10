e74c1ad355ae7145eb4b2b841a1b52852a8c36d8
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-10 19:15:14 +0200

    Split combination operator test

--- test/Stones/combinationTest.cpp
+++ test/Stones/combinationTest.cpp
@@ -4,20 +4,27 @@
 
 using namespace Combination;
 
-TEST(CombinationTest, ComparisonOperator) {
-    CardCombination c1 = CardCombination(Type::COLORED_SERIE, 10);
-    CardCombination c2 = CardCombination(Type::SUM, 12);
-    EXPECT_TRUE(c2 < c1);
-    EXPECT_FALSE(c1 < c2);
-    EXPECT_FALSE(c1 == c2);
+TEST(CombinationTest, ComparisonOperator_DifferentTypes) {
+    CardCombination strongCombination = CardCombination(Type::COLORED_SERIE, 10);
+    CardCombination weakCombination = CardCombination(Type::SUM, 12);
+    EXPECT_TRUE(weakCombination < strongCombination) << "Combination type comparison failure.";
+    EXPECT_FALSE(strongCombination < weakCombination) << "Combination type comparison failure.";
+    EXPECT_FALSE(strongCombination == weakCombination) << "Combination type comparison failure.";
+}
 
-    c2 = CardCombination(Type::COLORED_SERIE, 11);
-    EXPECT_FALSE(c2 < c1);
-    EXPECT_TRUE(c1 < c2);
-    EXPECT_FALSE(c1 == c2);
+TEST(CombinationTest, ComparisonOperator_SameTypeDifferentCardValues) {
+    CardCombination weakCombination = CardCombination(Type::COLORED_SERIE, 11);
+    CardCombination strongCombination = CardCombination(Type::COLORED_SERIE, 12);
 
-    c2 = CardCombination(Type::COLORED_SERIE, 10);
-    EXPECT_FALSE(c2 < c1);
-    EXPECT_FALSE(c1 < c2);
-    EXPECT_TRUE(c1 == c2);
+    EXPECT_TRUE(weakCombination < strongCombination) << "Combination, same type comparison failure.";
+    EXPECT_FALSE(strongCombination < weakCombination) << "Combination, same type comparison failure.";
+    EXPECT_FALSE(strongCombination == weakCombination) << "Combination, same type comparison failure.";
+}
+
+TEST(CombinationTest, CombinationOperator_EqualCombination) {
+    CardCombination firstCombination = CardCombination(Type::COLORED_SERIE, 12);
+    CardCombination secondCombination = CardCombination(Type::COLORED_SERIE, 12);
+    EXPECT_FALSE(firstCombination < secondCombination) << "Combination, equality comparison failure.";
+    EXPECT_FALSE(secondCombination < firstCombination) << "Combination, equality comparison failure.";
+    EXPECT_TRUE(firstCombination == secondCombination) << "Combination, equality comparison failure.";
 }
