62aad30c4131d17ea29066dffc50b6a54e8bbbe3
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-13 19:29:52 +0200

    Claim condition on Stone

--- src/Stones/stone.h
+++ src/Stones/stone.h
@@ -1,9 +1,14 @@
 #ifndef STONE_H
 #define STONE_H
 
+#include "combination.h"
+
 namespace Stone {
 
-class Stone {};
+class Stone {
+  private:
+    std::vector<Combination::Type> eligibleCombination;
+};
 } // namespace Stone
 
 #endif
