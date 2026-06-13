76ecc352ea24306bbe71b273c42212be0ece42ac
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-13 21:44:40 +0200

    Add base fields for Stone class

--- src/Stones/stone.h
+++ src/Stones/stone.h
@@ -1,13 +1,22 @@
 #ifndef STONE_H
 #define STONE_H
 
+#include <map>
+#include <optional>
+#include <string>
+#include <vector>
+
 #include "combination.h"
+#include "playerId.h"
+#include "side.h"
 
 namespace Stone {
 
 class Stone {
   private:
-    std::vector<Combination::Type> eligibleCombination;
+    std::vector<Combination::Type> eligibleCombinations;
+    std::map<Agent::PlayerID, Side> playerSides;
+    std::optional<Agent::PlayerID> claimedBy;
 };
 } // namespace Stone
 
