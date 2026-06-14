e002224a265b5efae0e5b7f5bc07d09860dd39ae
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-14 21:26:03 +0200

    Define PlayerId class tests

--- test/Player/PlayerId/playerIdTest.cpp
+++ test/Player/PlayerId/playerIdTest.cpp
@@ -1,3 +1,19 @@
 #include <gtest/gtest.h>
 
 #include "playerId.h"
+
+TEST(PlayerIdTest, Comparison_SameHashSource) {
+    Agent::PlayerID idA = Agent::PlayerID();
+    Agent::PlayerID idB = Agent::PlayerID();
+
+    EXPECT_EQ(idA, idB);
+    EXPECT_EQ(idB, idA);
+}
+
+TEST(PlayerIdTest, Comparison_DifferentHashSource) {
+    Agent::PlayerID idA = Agent::PlayerID("Test1");
+    Agent::PlayerID idB = Agent::PlayerID("Test2");
+
+    EXPECT_NE(idA, idB);
+    EXPECT_NE(idB, idA);
+}
