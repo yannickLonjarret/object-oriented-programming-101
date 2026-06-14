77e0c376eb1fc9ad61b9da37a6445a3e59b32579
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-14 22:18:00 +0200

    Define claim methods, preliminary work

--- src/Stones/stone.cpp
+++ src/Stones/stone.cpp
@@ -1,1 +1,22 @@
 #include "stone.h"
+
+namespace Stone {
+
+bool Stone::canPlayerClaim(const Agent::PlayerID& player) const {
+    const auto it = playerSides.find(player);
+
+    if (it == playerSides.cend()) [[unlikely]] {
+        return false;
+    }
+    return it->second.isComplete();
+}
+
+void Stone::claimStoneAs(const Agent::PlayerID& player) {
+    if (!canPlayerClaim(player)) {
+        // TODO: Add a better claim failure handler
+        return;
+    }
+    // TODO: Claim logic
+}
+
+} // namespace Stone
--- src/Stones/stone.h
+++ src/Stones/stone.h
@@ -17,6 +17,13 @@ class Stone {
     std::vector<Combination::Type> eligibleCombinations;
     std::map<Agent::PlayerID, Side> playerSides;
     std::optional<Agent::PlayerID> claimedBy;
+
+  public:
+    inline bool isClaimed() const noexcept { return claimedBy.has_value(); }
+
+    bool canPlayerClaim(const Agent::PlayerID& player) const;
+
+    void claimStoneAs(const Agent::PlayerID& player);
 };
 } // namespace Stone
 
