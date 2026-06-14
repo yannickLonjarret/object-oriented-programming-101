c7966271ae058bd33e6c42156ae8266251fabe95
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-14 16:55:05 +0200

    Define Side class behavior

--- src/Stones/Side/side.h
+++ src/Stones/Side/side.h
@@ -1,8 +1,36 @@
 #ifndef SIDE_H
 #define SIDE_H
 
+#include <format>
+#include <stdexcept>
+#include <vector>
+
+#include "card.h"
+#include "playerId.h"
+
 namespace Stone {
-class Side {};
+class Side {
+  private:
+    Agent::PlayerID owner;
+    size_t maxPlayableCardCount;
+    std::vector<Cards::ClanCard> playedCards;
+
+  public:
+    Side(Agent::PlayerID id, size_t max = 3) : owner(id), maxPlayableCardCount(max) { playedCards.reserve(maxPlayableCardCount); }
+
+    inline Agent::PlayerID getOwner() const noexcept { return owner; }
+
+    inline const std::vector<Cards::ClanCard>& getPlayedCards() const noexcept { return playedCards; }
+
+    inline bool isComplete() const noexcept { return playedCards.size() >= maxPlayableCardCount; }
+
+    inline void playCard(Cards::ClanCard&& cardToPlay) {
+        if (isComplete()) {
+            throw std::logic_error("Cannot play card, complete site.");
+        }
+        playedCards.push_back(std::move(cardToPlay));
+    }
+};
 
 } // namespace Stone
 
