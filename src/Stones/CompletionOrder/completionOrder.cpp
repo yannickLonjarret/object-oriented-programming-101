fca38558e8190ecdc92bb361c75509fe6fd2d2e2
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-22 21:58:58 +0200

    Define a get earliest between two PlayerId on CompletionOrder

--- src/Stones/CompletionOrder/completionOrder.cpp
+++ src/Stones/CompletionOrder/completionOrder.cpp
@@ -1,4 +1,5 @@
 #include "completionOrder.h"
+#include <stdexcept>
 
 namespace Stone {
 
@@ -7,5 +8,20 @@ void CompletionOrder::addCompletion(const Agent::PlayerID& player) {
         return;
     }
     order.push_back(player);
+}
+
+Agent::PlayerID CompletionOrder::getEarliest(const Agent::PlayerID& candidate, const Agent::PlayerID& contender) const {
+    const auto& candidateIterator = std::find(order.cbegin(), order.cend(), candidate);
+    const auto& contenderIterator = std::find(order.cbegin(), order.cend(), contender);
+
+    if (candidateIterator == order.cend() || contenderIterator == order.cend()) {
+        throw std::logic_error("Combination claim tie break error, candidate or contender not complete.");
+    }
+
+    if (candidateIterator < contenderIterator) {
+        return candidate;
+    } else {
+        return contender;
+    }
 }
 } // namespace Stone
--- src/Stones/CompletionOrder/completionOrder.h
+++ src/Stones/CompletionOrder/completionOrder.h
@@ -8,7 +8,8 @@ class CompletionOrder {
     std::deque<Agent::PlayerID> order;
 
   public:
-    inline Agent::PlayerID CompletionOrder::getFirstToComplete() const noexcept { return order.front(); }
+    inline Agent::PlayerID getFirstToComplete() const noexcept { return order.front(); }
+    Agent::PlayerID getEarliest(const Agent::PlayerID& candidate, const Agent::PlayerID& contender) const;
     void addCompletion(const Agent::PlayerID& player);
 };
 
