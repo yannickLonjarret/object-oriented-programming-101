b3705f8edaef6e5c7606e6fff3bc9cb24204a037
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-06-23 20:12:11 +0200

    Remove useless getFirstToComplete method

--- src/Stones/CompletionOrder/completionOrder.h
+++ src/Stones/CompletionOrder/completionOrder.h
@@ -8,7 +8,6 @@ class CompletionOrder {
     std::deque<Agent::PlayerID> order;
 
   public:
-    inline Agent::PlayerID getFirstToComplete() const noexcept { return order.front(); }
     [[nodiscard]] Agent::PlayerID getEarliest(const Agent::PlayerID& candidate, const Agent::PlayerID& contender) const;
     void addCompletion(const Agent::PlayerID& player);
 };
